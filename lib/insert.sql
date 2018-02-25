a8219ff4f08b9118116e468b6fa5befa968e38bf
sinclairstudy <sinclairstudy@gmail.com>
2018-02-25 13:15:58 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,58 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (
+  'Mr. Chocolate',
+  23,
+  'M',
+  'Brown',
+  'Smelly',
+  1
+),(
+  'Rowdy',
+  4,
+  'M',
+  'Green',
+  'Tranquil',
+  1
+),(
+  'Tabitha',
+  9,
+  'F',
+  'Brown',
+  'Pleasant',
+  1
+),(
+  'Sergeant Brown',
+  10,
+  'F',
+  'Black',
+  'Amicable',
+  1
+),(
+  'Melissa',
+  13,
+  'F',
+  'Brown',
+  'Rambunctious',
+  1
+),(
+  'Grinch',
+  13,
+  'M',
+  'Black',
+  'Caustic',
+  1
+),(
+  'Wendy',
+  123,
+  'F',
+  'Black',
+  'Smelly',
+  1
+),(
+  NULL,
+  1000,
+  'M',
+  'Brown',
+  'Murderous',
+  1
+);
