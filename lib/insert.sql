f5533cfd506866d9891a8566d00808e3c47a0fae
Ray Blasko <mray6288@gmail.com>
2018-02-26 09:07:17 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+insert into bears (name, age, gender, color, temperament, alive) values ('Mr Chocolate',5,'M','brown','angry',1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Rowdy',5,'M','brown','angry',1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Tabitha',5,'M','brown','angry',1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Sergeant Brown',5,'M','brown','angry',1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Melissa',5,'M','brown','angry',1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Grinch',5,'M','brown','angry',1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Wendy',5,'M','brown','angry',1);
+insert into bears (name, age, gender, color, temperament, alive) values (null,5,'M','brown','angry',1);
